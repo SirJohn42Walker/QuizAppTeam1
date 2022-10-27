2fe1e8f8fe2b9637ad64e722328a3b315a479817
Robert Schurig <robertschurig@yahoo.com>
2022-10-27 23:29:43 +0200

    Added a page and started a function

---
+++ JS/new-card.js
@@ -0,0 +1,20 @@
+// Lesen der Eingaben.
+const form = document.querySelector('[data-js="form"]');
+const questionInput = document.querySelector('[data-js="input-question"]');
+const answerInput = document.querySelector('[data-js="input-answer"]');
+const tagInput = document.querySelector('[data-js="input-tag"]');
+
+// Übermitteln der Eingaben
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  questionInput = document.createElement();
+  answerInput = document.createElement();
+  tagInput = document.createElement();
+
+  const newCard = document.createElement("li");
+  newCard.append();
+});
