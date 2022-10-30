818bcd7fcda04054c278cd7f87549ca395393d56
Timo Thees <timo.thees@gmx.net>
2022-10-30 09:56:19 +0100

    added hide function to hide-question-button

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,28 @@
+// bookmark functionality
+
 const bookmark = document.querySelector('[data-js="bookmark"]');
 
 bookmark.addEventListener("click", () => {
   if (bookmark.value.class === "bookmarks_unclicked") {
     bookmark.value.src = "/icons/lesezeichen-schwarz.png";
     bookmark.value.alt = "You saved this question in your bookmarks";
-    bookmark.value.class = "";
-    bookmark.value.class = "bookmarks_clicked";
+    bookmark.classList = "";
+    bookmark.classList = "bookmarks_clicked";
   } else {
     bookmark.value.src = "/icons/lesezeichen-schwarz.png";
     bookmark.value.alt = "Bookmark this question";
-    bookmark.value.class = "";
-    bookmark.value.class = "bookmarks_unclicked";
+    bookmark.classList = "";
+    bookmark.classList = "bookmarks_unclicked";
   }
+});
+
+//button to show or hide answer functionality
+
+const showAnswerButton = document.querySelector(
+  '[data-js="showanswer_button"]'
+);
+const questionText = document.querySelector('[data-js="question_text"]');
+
+showAnswerButton.addEventListener("click", () => {
+  questionText.classList.toggle("hidden");
 });
