403c95f29a40fa7623afd96fc81e625c959dfe69
Timo Thees <timo.thees@gmx.net>
2022-10-31 12:41:35 +0100

    Am stand Main angeglichen

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,17 @@
 // bookmark functionality
 
 const bookmark = document.querySelector('[data-js="bookmark"]');
+console.log(bookmark.classList);
 
 bookmark.addEventListener("click", () => {
-  if (bookmark.classList === "bookmarks_unclicked") {
-    bookmark.src = "/icons/lesezeichen-schwarze-form.png";
+  if (bookmark.classList.contains("bookmarks_unclicked")) {
+    bookmark.src = "./icons/lesezeichen-schwarze-form.png";
     bookmark.alt = "You saved this question in your bookmarks";
     bookmark.classList.remove("bookmarks_unclicked");
     bookmark.classList.add("bookmarks_clicked");
+    console.log("test");
   } else {
-    bookmark.src = "/icons/lesezeichen-weiss.png";
+    bookmark.src = "./icons/lesezeichen-weiss.png";
     bookmark.alt = "Bookmark this question";
     bookmark.classList.remove("bookmarks_clicked");
     bookmark.classList.add("bookmarks_unclicked");
